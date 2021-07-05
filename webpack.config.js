86148df78ecd806c6d8de682ea9afdd5066d22ef
AlexBizPlus <alexeynejnov@gmail.com>
2021-07-05 21:44:40 +0700

    [FIX] testing v.3

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.tsx'),
+  entry: path.resolve(__dirname, 'src/index.ts'),
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'public'),
