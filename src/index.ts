d83ac34b3d460278e09cc221c956197a95cf85bd
AlexBizPlus <alexeynejnov@gmail.com>
2021-07-05 22:05:19 +0700

    [FIX] testing v.4

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+/* eslint-disable import/no-useless-path-segments */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
-import './scss/index.scss';
+import App from '../src/components/App/App';
+import '../src/scss/index.scss';
 
 ReactDOM.render(React.createElement(App), document.querySelector('#root'));
