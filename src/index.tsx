99cffdf1ee0ea888f1c0d04269c2d583a7dbd78e
AlexBizPlus <alexeynejnov@gmail.com>
2021-07-05 21:19:47 +0700

    [FEAT] testing

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import './scss/index.scss';
 
+// eslint-disable-next-line no-console
+console.log(`main.js`);
+
 ReactDOM.render(React.createElement(App), document.querySelector('#root'));
